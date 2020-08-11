e401c45beb5879be695c91fcf2d302d506ef5d7f
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2020-08-11 15:06:26 -0400

    Add files via upload

---
+++ SelfGlow/SelfGlow/SelfGlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfGlow
+//
+//  Created by Carly Wenig on 8/11/20.
+//  Copyright © 2020 Carly Wenig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
